122ead4bf6007b60159f3a52ce455c207cd33513
FAL <falworks.contact@gmail.com>
2021-01-16 14:52:46 +0900

    refactor(build): add comment

--- scripts/post-transpile.js
+++ scripts/post-transpile.js
@@ -1,6 +1,8 @@
 import * as fs from "fs";
 import { libDirNode, libDirNodeEsmTmp } from "./paths.js";
 
+// move & rename lib for Node esm, then remove the old directory
+
 fs.promises
   .rename(`${libDirNodeEsmTmp}/index.js`, `${libDirNode}/index.mjs`)
   .then(() =>
